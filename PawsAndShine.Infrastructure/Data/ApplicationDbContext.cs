3b3eab137c7738f5dc4bb2ba71a2ee47c362bf5d
tingru00 <grundbergtindra@gmail.com>
2026-08-25 14:54:00 +0200

    Added ApplicationDbContext.cs

---
+++ PawsAndShine.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PawsAndShine.Domain.Entities;
+
+namespace PawsAndShine.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Service> Services { get; set; }
+        public DbSet<ServiceOption> ServiceOptions { get; set; }
+
+    }
+}
