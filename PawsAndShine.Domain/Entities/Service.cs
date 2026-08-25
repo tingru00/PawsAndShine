bbd392722c09f495037965f50bcff92bbacff2c1
tingru00 <grundbergtindra@gmail.com>
2026-08-25 14:29:48 +0200

    Add Service and ServiceOption entities.

---
+++ PawsAndShine.Domain/Entities/Service.cs
@@ -0,0 +1,11 @@
+﻿namespace PawsAndShine.Domain.Entities
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<ServiceOption> Options { get; set; } = new List<ServiceOption>();
+
+    }
+}
