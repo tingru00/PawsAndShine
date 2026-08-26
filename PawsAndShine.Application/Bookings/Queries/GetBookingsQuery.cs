578edefb859c1bec39c21eafded18d9a10c34382
tingru00 <grundbergtindra@gmail.com>
2026-08-26 13:56:28 +0200

    added getbookingsquery and handler

---
+++ PawsAndShine.Application/Bookings/Queries/GetBookingsQuery.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+using PawsAndShine.Domain.Entities;
+using PawsAndShine.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace PawsAndShine.Application.Bookings.Queries
+{
+    public record GetBookingsQuery : IRequest<List<Booking>>
+    {
+    }
+
+    public class GetBookingsQueryHandler : IRequestHandler<GetBookingsQuery, List<Booking>>
+    {
+        private readonly ApplicationDbContext _context;
+        public GetBookingsQueryHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Booking>> Handle(GetBookingsQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Bookings
+                .Include(b => b.Customer)
+                .Include(b => b.ServiceOption)
+                .ThenInclude(so => so.Service)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
