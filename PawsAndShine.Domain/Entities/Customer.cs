299c403f14f76e2eb4b83356fc94cd499ae619b4
tingru00 <grundbergtindra@gmail.com>
2026-08-26 10:54:09 +0200

    added entities booking and customer

---
+++ PawsAndShine.Domain/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PawsAndShine.Domain.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public List<Booking> Bookings { get; set; } = new List<Booking>();
+
+    }
+}
