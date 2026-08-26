6919fdbf550bfc92489e364f092ebf2026ef8dc7
tingru00 <grundbergtindra@gmail.com>
2026-08-26 09:58:47 +0200

    add GetServicesQuery and handler

---
+++ PawsAndShine.Application/Services/Queries/GetServicesQuery.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using PawsAndShine.Infrastructure.Data;
+using PawsAndShine.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace PawsAndShine.Application.Services.Queries
+{
+    public record GetServicesQuery : IRequest<List<Service>>
+    {  
+    }
+
+    public class GetServicesQueryHandler : IRequestHandler<GetServicesQuery, List<Service>>
+    {
+        private readonly ApplicationDbContext _context;
+        public GetServicesQueryHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Service>> Handle(GetServicesQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Services
+                .Include(s => s.Options)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
