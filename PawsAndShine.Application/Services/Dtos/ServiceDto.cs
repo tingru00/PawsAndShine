1148a20fdd723e53796d730da29328e7fc0bfdf4
tingru00 <grundbergtindra@gmail.com>
2026-08-26 18:30:29 +0200

    Added ServiceDto and ServiceOptionDto

---
+++ PawsAndShine.Application/Services/Dtos/ServiceDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PawsAndShine.Application.Services.Dtos
+{
+    public class ServiceDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<ServiceOptionDto> Options { get; set; } = new List<ServiceOptionDto>();
+    }
+}
---
+++ PawsAndShine.Application/Services/Dtos/ServiceOptionDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PawsAndShine.Application.Services.Dtos
+{
+    public class ServiceOptionDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int DurationInMinutes { get; set; }
+        public decimal Price { get; set; }
+    }
+}
