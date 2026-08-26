8747235a9da4eb7846c96863d64dc89fb0eddf69
tingru00 <grundbergtindra@gmail.com>
2026-08-26 14:19:14 +0200

    added createservicecommand and handler

---
+++ PawsAndShine.Application/Services/Commands/CreateServiceCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+using PawsAndShine.Domain.Entities;
+using PawsAndShine.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace PawsAndShine.Application.Services.Commands
+{
+    public record CreateServiceCommand : IRequest<int>
+    {
+        public string Name { get; init; } = string.Empty;
+        public string Description { get; init; } = string.Empty;
+    }
+
+    public class CreateServiceCommandHandler : IRequestHandler<CreateServiceCommand, int>
+    {
+        private readonly ApplicationDbContext _context;
+        public CreateServiceCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> Handle(CreateServiceCommand request, CancellationToken cancellationToken)
+        {
+            var service = new Service
+            {
+                Name = request.Name,
+                Description = request.Description
+            };
+            _context.Services.Add(service);
+            await _context.SaveChangesAsync(cancellationToken);
+            return service.Id;
+        }
+    }
+
+}
